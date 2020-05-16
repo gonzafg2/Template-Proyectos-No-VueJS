1f90596de0b77cbc7d0d3c49ab27843c93a172c8
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-16 04:08:59 -0400

    Agrego archivos base

---
+++ src/assets/js/main.js
@@ -0,0 +1,4 @@
+// import '../../assets/css/styles.css'
+import "../../assets/scss/style.scss";
+
+import thereAreTeenagers from "./check.js";
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); //to access built-in plugins
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+
+  entry: {
+    index: "./src/assets/js/main.js",
+    check: "./src/assets/js/check.js"
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "assets/js/[name].bundle.js",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/css/bundle.css",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
